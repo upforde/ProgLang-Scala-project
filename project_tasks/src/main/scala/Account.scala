438b2d2ba02bacd1f0ffc7be418dcd6bf9b12dac
upforde <upforde1@gmail.com>
2020-11-06 16:39:31 +0100

    project task 1 complete. fixed build.sbt

--- project_tasks/src/main/scala/Account.scala
+++ project_tasks/src/main/scala/Account.scala
@@ -6,12 +6,21 @@ class Account(val bank: Bank, initialBalance: Double) {
 
     val balance = new Balance(initialBalance)
 
-    // TODO
     // for project task 1.2: implement functions
     // for project task 1.3: change return type and update function bodies
-    def withdraw(amount: Double): Unit = ???
-    def deposit (amount: Double): Unit = ???
-    def getBalanceAmount: Double       = ???
+    def withdraw(amount: Double): Either[Unit, String] = this.synchronized{
+        if (amount < 0 || amount > balance.amount)
+            return Right("The amount seems to be negative.")
+        Left(balance.amount -= amount)
+    }
+
+    def deposit (amount: Double): Either[Unit, String] = this.synchronized{
+        if (amount < 0)
+            return Right("The amount seems to be negative.")
+        Left(balance.amount += amount)
+    }
+    
+    def getBalanceAmount: Double       = this.synchronized{balance.amount}
 
     def transferTo(account: Account, amount: Double) = {
         bank addTransactionToQueue (this, account, amount)
--- project_tasks/src/main/scala/Transaction.scala
+++ project_tasks/src/main/scala/Transaction.scala
@@ -1,30 +1,29 @@
 import exceptions._
-import scala.collection.mutable
+import scala.collection.mutable._
 
 object TransactionStatus extends Enumeration {
   val SUCCESS, PENDING, FAILED = Value
 }
 
 class TransactionQueue {
-
-    // TODO
     // project task 1.1
     // Add datastructure to contain the transactions
+    var queue = new Queue[Transaction]()
 
     // Remove and return the first element from the queue
-    def pop: Transaction = ???
+    def pop: Transaction = this.synchronized{queue.dequeue}
 
     // Return whether the queue is empty
-    def isEmpty: Boolean = ???
+    def isEmpty: Boolean = this.synchronized{queue.isEmpty}
 
     // Add new element to the back of the queue
-    def push(t: Transaction): Unit = ???
+    def push(t: Transaction): Unit = this.synchronized{queue.enqueue(t)}
 
     // Return the first element from the queue without removing it
-    def peek: Transaction = ???
+    def peek: Transaction = this.synchronized{queue.front}
 
     // Return an iterator to allow you to iterate over the queue
-    def iterator: Iterator[Transaction] = ???
+    def iterator: Iterator[Transaction] = this.synchronized{queue.iterator}
 }
 
 class Transaction(val transactionsQueue: TransactionQueue,
