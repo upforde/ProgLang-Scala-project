d0118d48b393341edc6b4860e813e26b9c912b56
upforde <upforde1@gmail.com>
2020-11-11 23:53:26 +0100

    Completed project

--- project_tasks/src/main/scala/Transaction.scala
+++ project_tasks/src/main/scala/Transaction.scala
@@ -40,18 +40,30 @@ class Transaction(val transactionsQueue: TransactionQueue,
     def doTransaction() = this.synchronized{
       // Keeping track of the success of both withdrawl
       // and deposit
-      var withdrawlSuccess = false
+      var transactionSuccess = false
       // Loop the allowed amount of times
       while(this.attempt != this.allowedAttemps){
         // Increment the attempt
         this.attempt += 1
         // If the withdrawl has yet to succeed
-        if (!withdrawlSuccess){
+        if (!transactionSuccess){
           // Try withdrawl
           from.withdraw(amount) match{
-            // If success ensure that withdrawl is not attempted again
-            // on subsequent attempts
-            case Left(()) => withdrawlSuccess = true
+            // If success, attempt to deposit money.
+            // Since deposit is restricted in the same way as withdrawl
+            // (fails on negative amount), if withdrawl fails, so will deposit.
+            // Therefore, if withdrawl succeeds
+            case Left(()) => to.deposit(amount) match{
+                // In case of success, increase the attempts 
+                // to the allowed amount and set the boolean
+                // to true
+                case Left(()) => {
+                  transactionSuccess = true
+                  this.attempt = this.allowedAttemps
+                }
+                // In case of failure, make sure the next if statement fails
+                case Right(string) => transactionSuccess = false
+              }
             // If failure
             case Right(string) => string match {
               // If the amount was negative, increase the attempts
@@ -64,22 +76,9 @@ class Transaction(val transactionsQueue: TransactionQueue,
             }
           }
         }
-        // Since deposit is restricted in the same way withdrawl (fails 
-        // on negative amount), if withdrawl fails, so will deposit.
-        // Therefore, if withdrawl succeeds
-        if (withdrawlSuccess){
-          // Attempt deposit
-          to.deposit(amount) match{
-            // In case of success, increase the attempts 
-            // to the allowed amount
-            case Left(()) => this.attempt = this.allowedAttemps
-            // In case of failure, make sure the next if statement fails
-            case Right(string) => withdrawlSuccess = false
-          }
-        }
       }
       // If the withdrawl has succeeded (meaning that the deposit succeeded as well)
-      if (withdrawlSuccess)
+      if (transactionSuccess)
         // Set the status of the transaction to SUCCESS
         this.status = TransactionStatus.SUCCESS
       // Otherwise, the transaction failed
